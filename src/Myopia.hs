f665b0a5901287434530a5e85e988b333109fa90
skykanin <3789764+skykanin@users.noreply.github.com>
2021-04-13 23:07:42 +0200

    Update game to use SDL abstraction library

--- src/Myopia.hs
+++ src/Myopia.hs
@@ -1,29 +1,51 @@
 module Myopia (run) where
 
-import Control.Monad (unless)
-import SDL
+import Data.Foldable (traverse_)
+import Graphics.SDL
+import SDL hiding (trace)
+import SDL.Image (loadTexture)
+import Prelude hiding (interact)
+
+data GameState = GameState
+  { fig1 :: [Texture]
+  , currentTex1 :: Int
+  , fig2 :: [Texture]
+  , currentTex2 :: Int
+  }
+
+draw :: GameState -> Picture
+draw (GameState f1 c1 f2 c2) =
+  Pictures [Sprite t1 sd1, Sprite t2 sd2]
+  where
+    t1 = f1 !! c1
+    t2 = f2 !! c2
+    sd1 = scaleBy 5 $ noTransform (P (V2 200 200))
+    sd2 = scaleBy 5 $ noTransform (P (V2 400 200))
+
+initState :: [Texture] -> [Texture] -> GameState
+initState as bs = GameState as 0 bs 0
+
+iterateWorld :: GameState -> GameState
+iterateWorld (GameState f1 c1 f2 c2) = GameState f1 nC1 f2 nC2
+  where
+    nC1 = (c1 + 1) `mod` 4
+    nC2 = (c2 + 1) `mod` 4
 
 run :: IO ()
 run = do
   initializeAll
   window <- createWindow "Myopia" defaultWindow
   renderer <- createRenderer window (-1) defaultRenderer
-  appLoop renderer
+  demonTex <- loadTextures renderer ["f0.png", "f1.png", "f2.png", "f3.png"]
+  heroTex <- loadTextures renderer ["h0.png", "h1.png", "h2.png", "h3.png"]
+  let startState = initState demonTex heroTex
+  interact renderer white startState draw iterateWorld (const id)
+  destroyTextures demonTex
+  destroyTextures heroTex
   destroyWindow window
 
-appLoop :: Renderer -> IO ()
-appLoop renderer = do
-  events <- pollEvents
-  let eventIsQPress event =
-        case eventPayload event of
-          KeyboardEvent keyboardEvent ->
-            keyboardEventKeyMotion keyboardEvent == Pressed
-              && keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
-          _ -> False
-      qPressed = any eventIsQPress events
-  rendererDrawColor renderer $= V4 0 200 0 255
-  clear renderer
-  rendererDrawColor renderer $= V4 255 0 0 255
-  fillRect renderer (Just (Rectangle (P (V2 0 0)) (V2 400 200)))
-  present renderer
-  unless qPressed (appLoop renderer)
+destroyTextures :: [Texture] -> IO ()
+destroyTextures = traverse_ destroyTexture
+
+loadTextures :: Renderer -> [FilePath] -> IO [Texture]
+loadTextures renderer = traverse (loadTexture renderer)
