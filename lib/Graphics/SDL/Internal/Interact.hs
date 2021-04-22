01c73a6b44f12f6e22696ad150637bd067644a1f
skykanin <3789764+skykanin@users.noreply.github.com>
2021-04-22 20:56:07 +0200

    Let texture cache live through each drawing of frame

--- lib/Graphics/SDL/Internal/Interact.hs
+++ lib/Graphics/SDL/Internal/Interact.hs
@@ -9,10 +9,11 @@
 module Graphics.SDL.Internal.Interact (interact) where
 
 import Control.Monad (unless)
+import Data.IORef (readIORef)
 import Graphics.SDL.Data.Color (Color)
 import Graphics.SDL.Data.Picture (Picture)
 import Graphics.SDL.Data.Window (WindowConfig, WindowTitle)
-import Graphics.SDL.Internal.DrawState (startDrawState)
+import Graphics.SDL.Internal.DrawState (DrawState, startDrawState, stateTextures)
 import Graphics.SDL.Internal.Render (drawPicture)
 import SDL (initializeAll, ($=))
 import SDL.Event (Event, EventPayload (..), eventPayload, pollEvents)
@@ -41,25 +42,28 @@ interact winTitle winConf bgColor world toPicture iterateWorld eventHandler = do
   initializeAll
   window <- createWindow winTitle winConf
   renderer <- createRenderer window (-1) defaultRenderer
-  loop renderer bgColor world toPicture iterateWorld eventHandler
+  drawState <- startDrawState
+  -- render state is preserved between frame renders so that textures aren't reloaded
+  loop renderer bgColor world drawState toPicture iterateWorld eventHandler
   quit
 
 loop ::
   Renderer ->
   Color ->
   world ->
+  DrawState ->
   (world -> Picture) ->
   (world -> world) ->
   (Event -> world -> world) ->
   IO ()
-loop renderer bgColor world toPicture iterateWorld eventHandler = do
+loop renderer bgColor world drawState toPicture iterateWorld eventHandler = do
   _ <- with 10 delay_
   clear renderer
-  drawState <- startDrawState
   drawPicture renderer drawState (toPicture world)
   rendererDrawColor renderer $= bgColor
   events <- pollEvents
   present renderer
+  --print =<< length <$> (readIORef $ stateTextures drawState)
   let quit = elem QuitEvent $ map eventPayload events
       newWorld = iterateWorld $ foldr eventHandler world events
-  unless quit $ loop renderer bgColor newWorld toPicture iterateWorld eventHandler
+  unless quit $ loop renderer bgColor newWorld drawState toPicture iterateWorld eventHandler
