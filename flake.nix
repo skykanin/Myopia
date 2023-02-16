63192c3f92d058b8ad29d87d573f4ecdf9d42e69
skykanin <3789764+skykanin@users.noreply.github.com>
2023-02-16 21:15:27 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,49 +1,50 @@
 {
   description = "Package build and dev environment for Myopia";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix/master";
-  outputs = { self, haskellNix, nixpkgs }: {
-    # setup derivation for x86_64-linux
-    defaultPackage.x86_64-linux =
-      let hn = haskellNix.legacyPackages.x86_64-linux.haskell-nix;
-          drv = hn.project {
-            src = hn.haskellLib.cleanGit {
-              name = "Myopia";
-              src = ./.;
-            };
-            compiler-nix-name = "ghc8104";
-          };
-      in drv.DraftGen;
-   
-    # setup devShell for x86_64-linux.
-    devShell.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      let
-        inherit (lib) makeLibraryPath;
-        hs = haskell.packages.ghc8104;
+
+  inputs = {
+    # Unofficial library of utilities for managing Nix Flakes.
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Nix Package set
+    nixpkgs.url = "github:nixos/nixpkgs?rev=d7705c01ef0a39c8ef532d1033bace8845a07d35";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachSystem (with flake-utils.lib.system; [x86_64-linux x86_64-darwin aarch64-darwin]) (system: let
+      compiler-version = "ghc944";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # setup devShell.
+      devShells.default = let
+        inherit (pkgs.lib) makeLibraryPath;
+        hs = pkgs.haskell.packages.${compiler-version};
+        hlib = pkgs.haskell.lib;
         tools = [
-          binutils-unwrapped
-          haskell-language-server
-          hlint
+          pkgs.binutils-unwrapped
+          pkgs.hlint
           hs.ghc
           hs.cabal-install
-          hs.ghcid
+          hs.cabal-plan
+          hs.cabal-fmt
+          hs.implicit-hie
+          (hlib.dontCheck hs.ghcid)
           hs.fourmolu
+          hs.haskell-language-server
         ];
-        libraries = [
-          pkg-config
-          SDL2
-          SDL2_gfx
-          SDL2_image
-        ];
+        libraries = with pkgs; [pkg-config SDL2 SDL2_gfx SDL2_image zlib];
         libraryPath = "${makeLibraryPath libraries}";
       in
-        mkShell {
+        pkgs.mkShell {
+          name = "dev shell";
           buildInputs = tools ++ libraries;
-          shellHook = ''
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libraryPath}"
-            export LIBRARY_PATH="${libraryPath}"
-          '';
+          LD_LIBRARY_PATH = "${libraryPath}";
+          LIBRARY_PATH = "${libraryPath}";
         };
-  };
+      formatter = pkgs.alejandra;
+    });
 }
