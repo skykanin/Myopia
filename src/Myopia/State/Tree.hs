d0a27f4a152a0c0b420e52b5666c7e55cd50ca38
skykanin <3789764+skykanin@users.noreply.github.com>
2023-05-15 21:16:15 +0200

    quad tree init module

---
+++ src/Myopia/State/Tree.hs
@@ -0,0 +1,21 @@
+-- |
+--    Module      : Myopia.State.Tree
+--    License     : GNU GPL, version 3 or above
+--    Maintainer  : skykanin <3789764+skykanin@users.noreply.github.com>
+--    Stability   : alpha
+--    Portability : portable
+--  Module defining the Tree and Points in it
+module Myopia.State.Tree (initTree) where
+
+import Myopia.QuadTree (Boundary (..), Point (..), QuadTree, V2 (..))
+import Myopia.QuadTree qualified as QT
+
+initTree :: QuadTree Double a
+initTree = QT.empty boundary 4
+  where
+    boundary =
+      Boundary
+        { center = P $ V2 600 400
+        , width = 550
+        , height = 400
+        }
